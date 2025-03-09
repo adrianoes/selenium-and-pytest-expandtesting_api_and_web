1c3a0d26cf307a8c404b6ca8f8ff46981042ade6
Adriano Santos <eustaquio.santos.adriano@gmail.com>
2025-03-09 14:57:37 +0100

    adicionando a opção headless

--- tests/ui/health_ui_test.py
+++ tests/ui/health_ui_test.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_health(driver):  # O fixture 'driver' será passado automaticamente pelo pytest
+def test_health(driver):  
     driver.get("https://practice.expandtesting.com/notes/app/")
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
     assert driver.title == "Notes React Application for Automation Testing Practice"
