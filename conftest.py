96e51538abede808e105b38fa9063dd4b690a5c9
Adriano Santos <eustaquio.santos.adriano@gmail.com>
2025-03-09 16:09:29 +0100

    fixing undetected webdriver

--- conftest.py
+++ conftest.py
@@ -10,8 +10,10 @@ def driver():
     options.add_argument("--start-maximized")
     options.add_argument("--headless")
     prefs = {
-        "profile.default_content_setting_values.ads": 2,
-        "profile.default_content_setting_values.popups": 2,
+        "profile.default_content_settings": {
+            "ads": 2,
+            "popups": 2
+        }
     }
     options.add_experimental_option("prefs", prefs)
     service = Service(ChromeDriverManager().install())
